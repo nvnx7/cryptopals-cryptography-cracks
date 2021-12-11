8b188c60f5936a2612c806fb9a68908d1da221fd
theNvN <thenvnsahu@gmail.com>
2021-12-12 00:19:03 +0530

    done fixed xor

--- src/set_1_basics.rs
+++ src/set_1_basics.rs
@@ -1,1 +1,2 @@
 pub mod convert_hex_to_base64;
+pub mod fixed_xor;
---
+++ src/set_1_basics/fixed_xor.rs
@@ -0,0 +1,26 @@
+use hex::{decode, encode};
+
+pub fn fixed_xor(hex1: &str, hex2: &str) -> String {
+    let bytes1 = decode(hex1).unwrap();
+    let bytes2 = decode(hex2).unwrap();
+
+    let xor_bytes: Vec<u8> = bytes1
+        .iter()
+        .zip(bytes2.iter())
+        .map(|(&b1, &b2)| b1 ^ b2)
+        .collect();
+    encode(xor_bytes)
+}
+
+#[cfg(test)]
+mod test {
+    use super::fixed_xor;
+    #[test]
+    fn xor() {
+        let hex1 = "1c0111001f010100061a024b53535009181c";
+        let hex2 = "686974207468652062756c6c277320657965";
+        let xor = "746865206b696420646f6e277420706c6179";
+
+        assert_eq!(fixed_xor(hex1, hex2), xor);
+    }
+}
