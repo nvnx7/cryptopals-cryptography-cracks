3cbb811d83e053754aca4d32645fccc7d3ccb2e4
theNvN <thenvnsahu@gmail.com>
2021-12-13 18:43:25 +0530

    renamed files in order

--- src/set_1_basics.rs
+++ src/set_1_basics.rs
@@ -1,3 +1,4 @@
-pub mod convert_hex_to_base64;
-pub mod fixed_xor;
-pub mod single_byte_xor_cypher;
+pub mod c1_convert_hex_to_base64;
+pub mod c2_fixed_xor;
+pub mod c3_single_byte_xor_cypher;
+pub mod c4_detect_single_char_xor;
